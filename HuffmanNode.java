b14d0eafa49e1dd044522a10836a8a331886ecee
Wamifawn <garrison.lazard@my.utsa.edu>
2025-10-31 16:04:06 -0500

    Create HuffmanNode.java

---
+++ HuffmanNode.java
@@ -0,0 +1,12 @@
+package src;
+class HuffmanNode {
+ char character;           
+ int frequency;       
+ HuffmanNode left, right; 
+ 
+ HuffmanNode(char character, int frequency) {
+     this.character = character;
+     this.frequency = frequency;
+     left = right = null;
+ }
+}
